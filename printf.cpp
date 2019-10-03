b40a8cd223b609da9727837afdc169190b1f97b8
song <songes0654@naver.com>
2019-10-03 22:01:51 +0900

    pintf

---
+++ printf.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+int main()
+{
+	printf("안녕하세요? \n");
+	printf("제 나이는 : %d", 24); //곧 25......
+	printf(" 키는 %d 입니다.", 175);
+
+	getchar();
+}
+*/
+/*
+int main()
+{
+	printf("저의 나이는 %d 이고 키는 %d 입니다.", 24, 175);
+	getchar();
+}
+
+*/
+
+
+
+	int main()
+
+	{
+		printf("%c", 'a');
+		getchar();
+	}
